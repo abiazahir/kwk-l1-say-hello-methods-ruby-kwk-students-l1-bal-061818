39688a17c16926eb9f57713f96ab566f715fbf58
abiazahir <abiazahir@hotmail.com>
2018-06-19 15:53:12 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,6 @@
 # Build your say_hello method here
-def say_hello (your_name)
+def say_hello (your_name="Ruby Programmer")
 puts "Hello #{your_name}!"
 end
-say_hello ("Ruby Programmer")
+say_hello ("Abia")
+say_hello
